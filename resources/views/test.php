c94b85691a340e57bca53aad56cf3c1fad8d5727
Kabir-Hossain <kabbirs96@gmail.com>
2024-11-18 16:32:36 +0600

    Mail Credential Update

--- resources/views/test.php
+++ resources/views/test.php
@@ -37,7 +37,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'hamko.tesst@gmail.com';                     //SMTP username
-    $mail->Password   = 'dahz cdud zpap pgwp';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // ENCRYPTION_SMTPS - Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
